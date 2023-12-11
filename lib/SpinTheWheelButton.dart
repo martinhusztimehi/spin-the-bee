7adc36f279547e2bdcf06a9256c3d167a081abec
Martin Huszti <martin.huszti@mehilainen.fi>
2023-12-11 16:19:36 +0200

    Added button

---
+++ lib/SpinTheWheelButton.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SpinTheWheelButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text("Spin the bee"));
+  }
+}
