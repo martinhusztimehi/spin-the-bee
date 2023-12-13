ec67089f940bbf6b89c3ae9c70e337537bb79c02
Martin Huszti <martin.huszti@mehilainen.fi>
2023-12-13 11:27:23 +0200

    Wheel button callback

--- lib/SpinTheWheelButton.dart
+++ lib/SpinTheWheelButton.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SpinTheWheelButton extends StatelessWidget {
+  final Function() onSpinButtonTapped;
+
+  const SpinTheWheelButton({super.key, required this.onSpinButtonTapped});
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {}, child: Text("Spin the bee"));
+    return ElevatedButton(
+        onPressed: onSpinButtonTapped, child: Text("Spin the bee"));
   }
 }
