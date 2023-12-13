e0538812afb5dcca95c2c2a944c9b71ce52fd736
Martin Huszti <martin.huszti@mehilainen.fi>
2023-12-13 10:18:32 +0200

    New component

--- lib/NameInput.dart
+++ lib/NameInput.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
-class NameInputWithState extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-}
-
 class NameInput extends StatelessWidget {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(labelText: "Enter name here"),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            width: 300,
+            child: TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                labelText: 'Enter your restaurant name',
+              ),
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            final restaurantName = myController.text;
+            print('Your restaurant name is $restaurantName');
+          },
+          child: Text('Save'),
+        ),
+      ],
     );
   }
 }
